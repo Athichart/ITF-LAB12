11ae22fc27da6d78527f3755cc063169fc3641c8
Athichart <73677899+Athichart@users.noreply.github.com>
2020-11-18 23:48:26 +0700

    Update show.php

--- show.php
+++ show.php
@@ -5,7 +5,7 @@
 <body>
 <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'itf1.mysql.database.azure.com', 'LABITF@itf1', 'kongooi45', 'itflab', 3306);
+mysqli_real_connect($conn, 'itf1.mysql.database.azure.com', 'LABITF@itf1', 'kongooi45!', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
@@ -13,9 +13,6 @@ if (mysqli_connect_errno($conn))
 $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
 <table width="600" border="1">
-    <div class="col-md-12">
-        <caption><a href="form.html" class="btn btn-primary"> เพิ่ม </a></caption>
-    </div>
   <tr>
     <th width="100"> <div align="center">Name</div></th>
     <th width="350"> <div align="center">Comment </div></th>
@@ -30,8 +27,8 @@ while($Result = mysqli_fetch_array($res))
     <td><?php echo $Result['Name'];?></div></td>
     <td><?php echo $Result['Comment'];?></td>
     <td><?php echo $Result['Link'];?></td>
-    <td><a href="change.php?id=<?php echo $Result['ID']; ?>" onclick="return confirm('ยืนยันการลบข้อมูล');" class="btn btn-danger">ลบ</a>
-                    <a href="modify.php?id=<?php echo $Result['ID']; ?>" class="btn btn-warning">แก้ไข</a>
+    <button type="button" onclick="alert('แก้ไขข้อมูล')">แก้ไข</button>
+    <button type="button" onclick="alert('ลบข้อมูล')">ลบ</button>
   </tr>
 <?php
 }
