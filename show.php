c9ef51db9b8e36474633b148a390a518d5265ef4
Athichart <73677899+Athichart@users.noreply.github.com>
2020-11-19 00:33:09 +0700

    Update show.php

--- show.php
+++ show.php
@@ -29,7 +29,7 @@ while($Result = mysqli_fetch_array($res))
     <td><?php echo $Result['Link'];?></td>
     <td><button type="button" onclick="alert('แก้ไขข้อมูล')">แก้ไข</button>
         <button type="button" onclick="alert('ลบข้อมูล')">ลบ</button>
-        <a href="form.html?id=<button type="button" onclick="alert('เพิ่มข้อมูล')">เพิ่ม</button>
+        <a href="form.html"><button type="button" onclick="alert('เพิ่มข้อมูล')">เพิ่ม</button></a>
     </td>
   </tr>
 <?php
