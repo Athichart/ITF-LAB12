7204c79211cef25632fed68e056dc3fc8f6178ed
Athichart <73677899+Athichart@users.noreply.github.com>
2020-11-19 23:25:22 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,14 +1,3 @@
-<html>
-<head>
-    <title>ITF Lab</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</head>
-    <body>
 <?php
 
 $conn = mysqli_init();
@@ -17,17 +6,21 @@ if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
+
+
 $name = $_POST['name'];
 $comment = $_POST['comment'];
 $link = $_POST['link'];
+
+
 $sql = "INSERT INTO guestbook (Name , Comment , Link) VALUES ('$name', '$comment', '$link')";
+
+
 if (mysqli_query($conn, $sql)) {
     echo "New record created successfully";
   } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
-  <a href="show.php?ID=<?php echo $Result['ID']?>" class="btn btn-info" onclick="alert('กลับสู่หน้าหลัก')">back</a>
+  
 mysqli_close($conn);
 ?>
-</body>
-</html>
